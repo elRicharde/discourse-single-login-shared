b9fbbf2e8d3c7aaf10f01ac59d9aa13ac872f1a5
elRicharde <52285099+elRicharde@users.noreply.github.com>
2026-01-19 10:08:49 +0100

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: discourse-single-login-shared
 # about: Allow only one concurrent login for a specific shared user
-# version: 0.2
+# version: 0.5
 # authors: Richard
 
 enabled_site_setting :single_login_shared_enabled
@@ -11,24 +11,28 @@ after_initialize do
       (SiteSetting.single_login_shared_username || "").strip
     end
 
-    def self.timeout_hours
-      (SiteSetting.single_login_shared_timeout_hours || 3).to_i
+    def self.timeout_seconds
+      ((SiteSetting.single_login_shared_timeout_hours || 3).to_i * 3600)
     end
 
     def self.shared_user?(user)
       user && user.username == shared_username
     end
 
-    def self.active_token_exists?(user)
-      cutoff = timeout_hours.hours.ago
+    def self.lock_key(user_id)
+      "single_login_shared:lock:user:#{user_id}"
+    end
 
-      UserAuthToken
-        .where(user_id: user.id)
-        .where("seen_at IS NOT NULL AND seen_at >= ?", cutoff)
-        .exists?
+    def self.locked?(user_id)
+      Discourse.redis.exists?(lock_key(user_id))
+    end
+
+    def self.touch_lock!(user_id)
+      Discourse.redis.setex(lock_key(user_id), timeout_seconds, "1")
     end
   end
 
+  # Login sauber blocken: JSON-Error + 4xx (damit kein "Unbekannter Fehler")
   ::SessionController.prepend Module.new {
     def create
       return super unless SiteSetting.single_login_shared_enabled
@@ -36,16 +40,26 @@ after_initialize do
       login = params[:login] || params[:username] || params.dig(:session, :login)
       user  = login.present? ? User.find_by_username_or_email(login) : nil
 
-      if ::SingleLoginShared.shared_user?(user) &&
-         ::SingleLoginShared.active_token_exists?(user)
-
-        render_json_error(
-          "Dieser Account ist bereits aktiv eingeloggt. Bitte später erneut versuchen."
+      if ::SingleLoginShared.shared_user?(user) && ::SingleLoginShared.locked?(user.id)
+        return render_json_error(
+          I18n.t("login.already_logged_in_single_session"),
+          status: 429
         )
-        return
       end
 
       super
+    ensure
+      u = (respond_to?(:current_user) ? current_user : nil)
+      if SiteSetting.single_login_shared_enabled && ::SingleLoginShared.shared_user?(u)
+        ::SingleLoginShared.touch_lock!(u.id)
+      end
     end
   }
+
+  # Inaktivitäts-Timeout: bei jedem Request verlängern
+  DiscourseEvent.on(:current_user) do |user|
+    if SiteSetting.single_login_shared_enabled && ::SingleLoginShared.shared_user?(user)
+      ::SingleLoginShared.touch_lock!(user.id)
+    end
+  end
 end
